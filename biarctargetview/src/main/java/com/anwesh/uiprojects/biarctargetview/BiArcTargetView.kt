8e468e2efd2bb0c6c34c3d418c6c1d21ed5302b5
Anwesh43 <anweshthecool0@gmail.com>
2019-06-17 09:14:55 +0530

    adding libraries and constants

--- biarctargetview/src/main/java/com/anwesh/uiprojects/biarctargetview/BiArcTargetView.kt
+++ biarctargetview/src/main/java/com/anwesh/uiprojects/biarctargetview/BiArcTargetView.kt
@@ -3,3 +3,23 @@ package com.anwesh.uiprojects.biarctargetview
 /**
  * Created by anweshmishra on 17/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val parts : Int = 2
+val lines : Int = 3
+val rotDeg : Float = 90f
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
