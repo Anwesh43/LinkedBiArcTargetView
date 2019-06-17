d50149a95a5d89f680768d6628f0c71e43104192
Anwesh43 <anweshthecool0@gmail.com>
2019-06-17 09:32:35 +0530

    adding drawBATNode canvas extension method

--- biarctargetview/src/main/java/com/anwesh/uiprojects/biarctargetview/BiArcTargetView.kt
+++ biarctargetview/src/main/java/com/anwesh/uiprojects/biarctargetview/BiArcTargetView.kt
@@ -33,3 +33,50 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a: Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawTargetArc(i : Int, sc : Float, size : Float, paint : Paint) {
+    val r : Float = size / 2
+    val sci : Float = sc.divideScale(i, parts)
+    val sf : Float = 1f - 2 * i
+    save()
+    scale(sf, 1f)
+    drawArc(RectF(-r, -r, r, r), -90f, 180f * sci, false, paint)
+    restore()
+}
+
+fun Canvas.drawBiArc(sc : Float, size : Float, paint : Paint) {
+    for (j in 0..(parts - 1)) {
+        drawTargetArc(j, sc, size, paint)
+    }
+}
+
+fun Canvas.drawTargetLines(sc : Float, size : Float, paint : Paint) {
+    val offsetY : Float = -size / 2
+    var currDeg : Float = 0f
+    for (j in 0..(lines - 1)) {
+        val scj : Float = sc.divideScale(j, lines)
+        currDeg += rotDeg * scj
+        save()
+        rotate(currDeg)
+        drawLine(0f, offsetY, 0f, 2 * offsetY, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawBATNode(i : Int, scale : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBiArc(sc1, size, paint)
+    drawTargetLines(sc2, size, paint)
+    restore()
+}
