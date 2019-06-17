4aaac85c36dd687eab98effd62bda600baecf020
Anwesh43 <anweshthecool0@gmail.com>
2019-06-17 09:36:49 +0530

    adding BiArcTargetView class

--- biarctargetview/src/main/java/com/anwesh/uiprojects/biarctargetview/BiArcTargetView.kt
+++ biarctargetview/src/main/java/com/anwesh/uiprojects/biarctargetview/BiArcTargetView.kt
@@ -80,3 +80,21 @@ fun Canvas.drawBATNode(i : Int, scale : Float, paint : Paint) {
     drawTargetLines(sc2, size, paint)
     restore()
 }
+
+class BiArcTargetView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
